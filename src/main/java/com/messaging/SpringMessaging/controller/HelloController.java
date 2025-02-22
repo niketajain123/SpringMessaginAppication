9711d27c9e56ed32fe93b215bb6d9e7b9b0bc1f7
niketajain123 <niketajain236@gmail.com>
2025-02-22 11:16:30 +0530

    [Niketa Jain]Add. hello mark added.

--- src/main/java/com/messaging/SpringMessaging/controller/HelloController.java
+++ src/main/java/com/messaging/SpringMessaging/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.messaging.SpringMessaging.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String hello(@RequestParam(value = "name", defaultValue = "Guest") String name) {
+        return "Hello, " + name + " from BridgeLabz";
     }
 }
