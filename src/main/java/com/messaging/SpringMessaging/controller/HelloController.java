83989137e2fa207c55c27c3a9576b4214466f4e7
niketajain123 <niketajain236@gmail.com>
2025-02-22 11:29:41 +0530

    [Niketa Jain]Add. hello method with path variable added.

--- src/main/java/com/messaging/SpringMessaging/controller/HelloController.java
+++ src/main/java/com/messaging/SpringMessaging/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.messaging.SpringMessaging.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,10 @@ public class HelloController {
 
     @GetMapping("/hello/query")
     public String hello(@RequestParam(value = "name", defaultValue = "Guest") String name) {
+        return "Hello, " + name + " from BridgeLabz";
+    }
+    @GetMapping("/hello/param/{name}")
+    public String helloWithPathVariable(@PathVariable String name) {
         return "Hello, " + name + " from BridgeLabz";
     }
 }
