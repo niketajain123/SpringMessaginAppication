9ba8d2d1fe36737453e1cc95984ed52558aafcc6
niketajain123 <niketajain236@gmail.com>
2025-02-22 11:11:28 +0530

    [Niketa Jain]Add. Hello from bridgelabz added.

---
+++ src/test/java/com/messaging/SpringMessaging/SpringMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messaging.SpringMessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
